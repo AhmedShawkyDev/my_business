68a8068fb002c73bc5ad0aede61c93b9f2c76b00
unknown <ahmedshawqi@dubisign.com>
2026-08-16 01:54:28 +0300

    border

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,7 +2,7 @@ export default function Container({ children, className, id, animation }) {
   return (
     <div
       data-aos={animation}
-      className={`flex flex-col  mx-4 lg:mx-auto lg:max-w-md lg:w-full items-center justify-center mt-7 mb-7 ${className}`}
+      className={`flex flex-col  mx-4 lg:mx-auto lg:max-w-md lg:w-full items-center justify-center mt-7 mb-7 border border-[#343A43] ${className}`}
       id={id}
     >
       {children}
